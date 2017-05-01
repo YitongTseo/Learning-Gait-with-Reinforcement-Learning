6b33029cc38880a0975c591d15940a61e8cfada2
Kaleb Tseo <yitong.tseo@gmail.com>
2017-05-01 19:06:18 -0400

    now this is just a main method

--- reinforcementLearning/test.cc
+++ reinforcementLearning/test.cc
@@ -1,8 +1,11 @@
-#include "test.h"
+#pragma once
+#include "toyEnvironment.h"
+#include "qLearning.h"
 
-void test::run() {
-	toyEnvironment te();
-	qLearning ql(te);
+int main() {
+	ToyEnvironment te;
+	te = ToyEnvironment();
+	qLearningAgent ql(te);
 	int counter = 10;
 
 	for (int i = 0; i <= counter; ++i){
@@ -10,22 +13,24 @@ void test::run() {
 			cout << "reset!\n";
 			te.reset();
 		}
-		//outside thing calls getAction, gets the action.
-		Action a = ql.getAction();
 
 		//Make sure to save the old state.
 		State oldState = te.getCurrentState(); //TODO: make sure this is pass by value.
 
+		//outside thing calls getAction, gets the action.
+		Action action = ql.getAction(oldState);
+
+	
 		//now we have state and action
 
 		//find the reward
 		float reward = te.getReward(oldState, action);
 
 		//and the new state. 
-		te.doAction(a);
-		float nextState = te.getCurrentState();
+		te.doAction(action);
+		State nextState = te.getCurrentState();
 
-		cout << "nextState: " << nextState << "oldState: " << oldState << "action: " << action <<" make sure these are not pass by reference \n";
+		cout << "nextState: " << &nextState << "oldState: " << &oldState << "action: " << action.move <<" make sure these are not pass by reference \n";
 
 		// cout << "beliefs: " << ql.belief
 
