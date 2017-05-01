2c510d803a47f6efd0aa109712d186bfe5f1fa31
Kaleb Tseo <yitong.tseo@gmail.com>
2017-05-01 19:05:22 -0400

    mostly reformats
    got rid of tuples too

--- reinforcementLearning/qLearning.h
+++ reinforcementLearning/qLearning.h
@@ -1,38 +1,38 @@
 #include <iostream>
-#include <tuple>
 #include <unordered_map>
 #include <vector>
 #include <algorithm>    // std::max
 
+using namespace std;
 
 class qLearningAgent {
 
 protected:
 	Environment environment;
-	float ALPHA = 0.9f;
-	float GAMMA = 0.1f;
-	float EPSILON = 0.0f;
-	//always have tuples
-	unordered_map<tuple<State&, Action&>, float> beliefDict;
+	const float ALPHA;
+	const float GAMMA;
+	const float EPSILON;
+	std::unordered_map<StateAction, float> beliefDict;
 
 
 public:
 	void updateBeliefs(const State& state,
-					   const Action$ action,
+					   const Action& action,
 					   const State& nextState,
-					   const float reward) override;
+					   const float reward);
 
-	qLearningAgent(Environment& e) {
+	qLearningAgent(Environment& e): ALPHA(0.9f), GAMMA(0.1f), EPSILON(0.0f) {
 		environment = e;
 	}
 
-	Action getAction(const State& state) override;
+	Action getAction(const State& state);
 
-	void qLearningAgent::computeValueFromQValues(const State& state) override;
+	void computeValueFromQValues(const State& state);
 
-	Action qLearningAgent::computeActionFromQValues(const State& state) override;
+	Action computeActionFromQValues(const State& state);
 
-	float qLearningAgent::getQValue(const State& state, const Action& action) override;
+	float getQValue(const State& state, const Action& action);
 	
-
+	~qLearningAgent();
 };
+
