75917335aa29a6b01d7cafa1dfd4e7a8f62f2fe5
David Burt <David@dburts-Macintosh.local>
2017-05-16 14:08:40 -0400

    msg

--- reinforcementLearning/wheeledRobotEnvironment2.h
+++ reinforcementLearning/wheeledRobotEnvironment2.h
@@ -3,10 +3,9 @@
 #include <unordered_map>
 #include <vector>
 #include <math.h>
-//Header file for the qlearning.cc
 
-//Takes in current state and reward state
 
+//States include the velocity of both wheels and the direction the robot is facing (in the xy) plane
 class State {
 public:
 	float leftWheelVelocity, rightWheelVelocity, robotOrientation;
@@ -16,7 +15,9 @@ public:
 	~State() {}
 };
 
-
+//Actions are accelerating or decelerating either wheel by 1 unit, so they are ordered pairs
+//A unit is defined by looking at the specified range and dividing it into 
+//the given number of buckets
 class Action {
 public:
 	//these increments are by index. only possible values; +1, 0, -1
@@ -27,6 +28,7 @@ public:
 	~Action() {}
 };
 
+//State actions are state action pairs, the class was needed so they could be hashed
 class StateAction {
 public: 
 	State state;
@@ -50,7 +52,7 @@ public:
 namespace std{
 
 template<>
-
+//Define a hash on state action pairs
 struct hash<StateAction>
   {
     std::size_t operator()(const StateAction& sa) const
@@ -58,11 +60,8 @@ struct hash<StateAction>
       using std::size_t;
       using std::hash;
 
-      // Compute individual hash values for first,
-      // second and third and combine them using XOR
-      // and bit shifting:
-
-      //note I have no idea if this is a good hash function. Sorry in advance. -Yitong
+      // hash function uses bitshifting a XORs to combine has function of
+      // individual floats appearin the state, action pair
       return ((hash<float>()(sa.state.leftWheelVelocity)
                ^ (hash<float>()(sa.state.rightWheelVelocity) << 1)) >> 1)
       		   ^ ((hash<float>()(sa.state.robotOrientation) << 5) >> 2)
@@ -72,46 +71,53 @@ struct hash<StateAction>
   };
 }
 
+//A generic environement class with methods to be overwritten, state values are intialized to be bad
+// so if not overwritten properly it will be clear
 class Environment {
 
 public:
-  virtual State getCurrentState() {return State(-99999.0f, 99999.0f, 91919191.0f);}
+  virtual State getCurrentState() {return State(-99999.0f, 99999.0f, 999999.0f);}
   virtual std::vector<Action>  getPossibleActions() { return std::vector<Action>(); }
   virtual void doAction(Action& a) {}
-  virtual float getReward(State& state, Action& a) {return 12321.0f;}
   virtual void reset() {}
-  virtual bool isTerminal(int robotXPos, int robotZPos) {return false;} //Could implement isTerminal wihout making it virtual, but we leave the option open for now
+  virtual bool isTerminal(int robotXPos, int robotZPos) {return false;} 
   Environment() {};
   virtual ~Environment() {};
 };
 
-
+//The environment class for a 2-wheeled-learner 
 class WheelsEnvironment : public Environment {
 
 protected: 
 	State state;
 	float maxVelocity;
 
-	//number of buckets to put velocity in range [-maxVelocity, maxVelocity]
+	//number of buckets to divide the velocity range [-maxVelocity, maxVelocity] in to
 	int numBuckets;
 
-	float reward;
-
 	//compute this once in the constructor to avoid confusion
+	// this should approximately (due to rounding) correspond to both wheels being still
 	int midVelocityIndex;
 
 public:
 	std::vector<float> wheelBuckets;
 	//e.g.
-	//if mv = 1 and nb = 5:
-	// -1   -0.5    0     0.5     1
-	//  0     1     2      3      4
+	//if maxVelocity = 1 and nb = 5:
+	// -1   -0.5    0     0.5     1   	Velocity
+	//  0     1     2      3      4  	Index
 	//midVelocityIndex = 2
 
 
 	int leftVelocityIndex, rightVelocityIndex;
+
 	WheelsEnvironment(float mv = 1, int nb = 5) : state(0.0f, 0.0f, 0.0f), maxVelocity(mv), numBuckets(nb) {
+		//The speed to increment by is the range of velocities dided by the number of 
+		//buckets desired (minus 1 since it includes both ends) Note minVelocity is defined to be -maxVelocity.
 		const float wheelIncrement = 2.0f * maxVelocity / (float(numBuckets) - 1);
+
+		//Define all possible velocities the wheels can take on, it is important to do this 
+		//rather than incrementing velocities directly by the increment to avoid rounding errors leading 
+		//to the same states appearing different
 		float bucket;
 		for (int i = 0; i < numBuckets; ++i) {
 			bucket = -float(maxVelocity) + (i * wheelIncrement);
@@ -127,28 +133,19 @@ public:
 		state.rightWheelVelocity = wheelBuckets.at(rightVelocityIndex);
 	}
 
-	//state is old state (before the action is taken)
-	float getReward(State& state, Action& a) {
-		return reward;
-	}
-
 	//returns state not by pointer but by value.
 	virtual State getCurrentState() {
 		return state;
 	}
 
-	void setRobotReward(float rv) {
-		//round rv to the nearest 10s decimal place
-		reward = rv;
-	}
-
 	void setRobotOrientation(float ro) {
 		//round rv to the nearest 10s decimal place
-		//TODO: rounding 0 - 2pi like this is sloppy as hell.
 		ro = roundf(ro * 10.0f) / 10.0f;
 		state.robotOrientation = ro;
 	}
 
+	//returns a list of pairs indicating which wheel can have velocity incremented in the positive direction 
+	//and which wheels can be accelerated in the negative direct (0,0) is always a possible action
 	virtual std::vector<Action> getPossibleActions() {
 		std::vector<Action> actions;
 		for(int i = -1; i <= 1; ++i) {
@@ -166,22 +163,28 @@ public:
 		return actions;
 	}
 
+
 	void doAction(Action& a) {
 		leftVelocityIndex += a.leftIncrement;
 		rightVelocityIndex += a.rightIncrement;
 
-		if (leftVelocityIndex >= numBuckets || 
-			leftVelocityIndex < 0 ||
-			rightVelocityIndex >= numBuckets || 
-			rightVelocityIndex < 0) {
-		   std::cout << "uh oh!!! our velocity indexes are out of bounds!";
-			return;
-		}
+		//This was a check to make sure getPossibleActions was behaving correctly
+
+		// if (leftVelocityIndex >= numBuckets || 
+		// 	leftVelocityIndex < 0 ||
+		// 	rightVelocityIndex >= numBuckets || 
+		// 	rightVelocityIndex < 0) {
+		//    std::cout << "uh oh!!! our velocity indexes are out of bounds!";
+		// 	return;
+		// }
 		state.leftWheelVelocity = wheelBuckets.at(leftVelocityIndex);
 		state.rightWheelVelocity = wheelBuckets.at(rightVelocityIndex);
 	}
 
-	//when reset is called. the robot is gonna have to also be teleported back to position.
+	//Neither of these were used in the turn_wheels learner. Since the ground plane is infinite and homogenous
+	//the robot position does not effect its states or actions in a meaningful way
+
+	//This resets internal states. When reset is called, the robot is has to be reset in Gazebo as well.
 	void reset() {
 		leftVelocityIndex = midVelocityIndex;
 		rightVelocityIndex = midVelocityIndex;
@@ -189,8 +192,9 @@ public:
 		state.rightWheelVelocity = wheelBuckets.at(rightVelocityIndex);
 	}
 
+	//Defines a state to be terminal if the robot leaves a 5 meter radius of the initial circle.
 	bool isTerminal(int robotXPos, int robotZPos) {
-		//pythagrom theorem!
+		//pythagorean theorem!
 		float distanceFromCenter = robotXPos * robotXPos + robotZPos * robotZPos;
 		float radius = 5.0f;
 		return radius > distanceFromCenter;
--- turn_wheels.cc
+++ turn_wheels.cc
@@ -42,11 +42,11 @@ namespace gazebo
       //store the pointers to the joints
       this->jointsVector = this->model->GetJoints();
 
-      std::cout<<"\n1";
+      //initialize a wheels environement to interact with
       we = WheelsEnvironment();
-      std::cout<<"2";
+
+      //construct a q learning agent with states, actions and rewards dicated by the wheeled environment
       ql = qLearningAgent(we);
-      std::cout<<"3\n";
 
       //set the method OnUpdate() as a listener. It'll be called every time step.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
@@ -57,53 +57,43 @@ namespace gazebo
   // Called by the world update start event
   public: void OnUpdate(const common::UpdateInfo & /*_info*/)
   {
-    // Apply a small linear velocity to the model.
-    //this->model->SetLinearVel(math::Vector3(0.06, 0, 0));
-
-    //     if (this->we.isTerminal(relativePosition.x, relativePosition.z)){
-//       this->we.reset();
-//       this->model.reset();//TODO!: teleport the model back to original position
-//     }
-
+  	//leftJoint is a pointer to the left wheel and rightJoint to the right wheel
     physics::JointPtr leftJoint = this->jointsVector[0];
     physics::JointPtr rightJoint = this->jointsVector[1];
 
-
+    //old State stores the orientation and velocities in the model as a state variable
     State oldState(this->we.getCurrentState());
+
+    //get the action determined by the Q-Learning algorithm from the Q-learner class based on old State
     Action action = this->ql.getAction();
 
+    //print statements to give information about current wheel velocities and changes in velocity
     cout << "\nleft wheel velocity " << oldState.leftWheelVelocity;
     cout << "\nleft wheel increment " << action.leftIncrement;
 
     cout << "\nright wheel velocity " << oldState.leftWheelVelocity;
     cout << "\nright wheel increment " << action.leftIncrement;
     
-    cout << "\n size of bucket" << we.wheelBuckets.size();
-
-    //find the reward
-    float reward = this->we.getReward(oldState, action);
-
-    cout << "\nreward " << reward;
-
     //update the environment
     this->we.doAction(action);
     cout << "\nleft wheel velocity " << oldState.leftWheelVelocity;
 
-
-    // leftJoint->SetVelocity(0, .7);
+    //get the new velocities in the model dictated by taking the action (i.e. accelerate wheels appropriately)
     State nextState = this->we.getCurrentState();
-    //move the actual joints in Gazebo
+
+    //update wheel speeds in Gazebo
     leftJoint->SetVelocity(0, nextState.leftWheelVelocity);
     rightJoint->SetVelocity(0, nextState.rightWheelVelocity);
 
 
 
-
+    //Get the models new velocity, position and orientation
     math::Vector3 relativeVelocity = this->model->GetRelativeLinearVel();
     math::Pose relativePose = this->model->GetRelativePose();
     math::Vector3 relativeRotation = relativePose.rot.GetAsEuler();
     math::Vector3 relativePosition = relativePose.pos;
 
+    //Print out the velocity, position and orientation to monitor progress
     std::cout << "\nrelative POSITION: x: " << relativePosition.x << " y: " 
       << relativePosition.y << " z: " << relativePosition.z;
 
@@ -114,11 +104,19 @@ namespace gazebo
 
     std::cout << "\n robotOrientation" << nextState.robotOrientation << endl;
 
-    //learn to move forward in the x direction
-    this->we.setRobotReward(relativePosition.x - abs(relativePosition.y));
+	// update the sample reward in the wheeled evironment,
+	//learn to move forward in the x direction
+	//reward is positive displacement in x direction, minus a penalty 
+	// for straying of course in the y-direction
+    float reward = relativePosition.x - abs(relativePosition.y);
+
+    //print the reward
+    cout << "\nreward " << reward;
+
+    //Update the wheeled environment so it know the new robot orientation
     this->we.setRobotOrientation(relativeRotation.z);
     
-    //then call update Beliefs with those arguments.
+    //call update Beliefs with the old State, the action taken, the new state and the reward 
     this->ql.updateBeliefs(oldState, action, nextState, reward);
 
   }
