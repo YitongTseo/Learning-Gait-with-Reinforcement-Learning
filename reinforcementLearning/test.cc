ed789e70e4e1ce83ce045359a980ce9ebd59c91f
Yitong Tseo <yitong@Yitongs-MacBook-Pro.local>
2017-05-16 15:51:40 -0400

    test.cc works now

--- reinforcementLearning/test.cc
+++ reinforcementLearning/test.cc
@@ -1,72 +1,8 @@
 //#pragma once
-#include "sixLeggedForceEnvironment.h"
-//#include "qLearning.h"
-//#include "qLearning.cpp"
+#include "toyEnvironment.h"
+#include "qLearning.cpp"
 
-int main() {
-  std::cout << "is this working?\n";
-  SixLegsForceEnvironment sle;
-  State oldState(sle.getCurrentState());
 
-  for(int i = 0; i < oldState.jointPos.size(); ++i ){
-     std::cout << "\nprint joint pos i  " << oldState.jointPos.at(i);
-  }
-
-  std::vector<float> positions;
-  // 0.3925
-  for(int i = 0; i < (oldState.jointPos.size() / 2); ++i ){
-     positions.push_back(2.0f);
-     positions.push_back(0.587f);
-  }  
-
-  std::cout<< "\ntime to set those positions! \n\n";
-  sle.setRobotPosition(positions);
-
-  sle.setRobotOrientationYPR(0.33f, 1.55f, .23f);
-
-  State newState(sle.getCurrentState());
-  for(int i = 0; i < newState.jointPos.size(); ++i ){
-     std::cout << "\nprint joint pos " << i << ": " << newState.jointPos.at(i);
-  }
-
-  std::cout << "\n yaw: " << newState.roboOrientation[0] << "  pitch: " << newState.roboOrientation[1] <<  "roll: " << newState.roboOrientation[2];
-
-  float legForce = 0.0f;
-  int jointIndex = 0;
-  std::vector<Action> actions = sle.getPossibleActions(jointIndex, legForce);
-  
-  for(int i = 0; i < actions.size(); ++i ){
-    std::cout << "\naction increment: " << actions.at(i).increment << " jointindex: " << actions.at(i).jointIndex;
-  }
-
-  std::cout << "\n isterminal: " << sle.isTerminal() << "\n";
-
-  sle.reset();
-  State newestState(sle.getCurrentState());
-  for(int i = 0; i < newestState.jointPos.size(); ++i ){
-     std::cout << "\nprint joint pos " << i << ": " << newestState.jointPos.at(i);
-  }
-
-  std::cout << "\n yaw: " << newestState.roboOrientation[0] << "  pitch: " << newestState.roboOrientation[1] <<  "roll: " << newestState.roboOrientation[2];
-
-  qLearningAgent ql(sle);
-
-  //print out of the beliefDict. for debugging purposes only.
-  // for(auto kv : ql.beliefDict) {
-  //   StateAction sa = kv.first;
-  //   float value = kv.second;
-  //   std::cout << "state: " << sa.state.position << " action: " << sa.action.move;
-  //   std::cout << " BELIEF:  " << value << "\n";
-  // }
-
-  // State newState(sle.getCurrentState());
-
-  // for(int i = 0; i < newState.jointPos.size(); ++i ){
-  //   std::cout << "\nnew state i "  << i << " "<< newState.jointPos.at(i);
-  // }
-}
-
-/*
 int main() {
   ToyEnvironment te;
   qLearningAgent ql(te);
@@ -105,4 +41,4 @@ int main() {
     std::cout << " BELIEF:  " << value << "\n";
   }
 }
-*/
+
