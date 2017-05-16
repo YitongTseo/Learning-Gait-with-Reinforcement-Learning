054549e02f32fd384a0a43da2553ee026826dca3
David Burt <David@dburts-Macintosh.local>
2017-05-16 14:30:59 -0400

    Commented QLearner header file

--- reinforcementLearning/qLearning.h
+++ reinforcementLearning/qLearning.h
@@ -9,29 +9,27 @@ class qLearningAgent {
 
 protected:
 	Environment *environment;
-	float ALPHA;
-	float GAMMA;
-	float EPSILON;
-	//std::unordered_map<StateAction, float> beliefDict;
+	float ALPHA; //Learning Rate, between 0 and 1, small gives a "longer memory", large changes beliefs more quickly
+	float GAMMA; //Discount Factor, between 0 and 1, we probably want it close to 1 to avoid the robot from making short
+				 // term gains that jeopardize stability etc...
+	float EPSILON; //Probability of randomly selecting an action, larger values lead to more exploration
 	float computeValueFromQValues();
 	Action computeActionFromQValues();
 	float getQValue(const Action& action);
 
 public:
-	//TODO: make this protected again.
 	std::unordered_map<StateAction, float> beliefDict;
 	void updateBeliefs(const State& state,
 			   const Action& action,
 			   const State& nextState,
 			   const float reward);
 
-	qLearningAgent(): ALPHA(0.9f), GAMMA(0.1f), EPSILON(0.1f){}
+	qLearningAgent(): ALPHA(0.1f), GAMMA(0.9f), EPSILON(0.1f){}
 
 	qLearningAgent(Environment& e, 
-				   float alpha = 0.9f, 
-				   float gamma = 0.1f, 
+				   float alpha = 0.1f, 
+				   float gamma = 0.9f, 
 				   float epsilon = 0.1f) : ALPHA(alpha), GAMMA(gamma), EPSILON(epsilon) {
-		//we want to store _the pointer_ to e, not the value of e.
 		environment = &e;
 	}
 
